259aa066d6c7de05623c49a6bccad20b3fa6d2a9
Karan Kumar <kk5214662@gmail.com>
2026-02-26 16:22:52 +0530

    feat(add):Create user card to display

--- Fetch_User/src/components/userCard.jsx
+++ Fetch_User/src/components/userCard.jsx
@@ -1,7 +1,6 @@
 function UserCard({ user }) {
   return (
     <div className="card">
-        <img  className="user_img" alt={user.id} />
       <h3 className="name">Name:{user.name}</h3>
       <p className="email">Email:{user.email}</p>
     </div>
